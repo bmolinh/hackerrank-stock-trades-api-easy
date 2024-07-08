85c1a4355707a38d35ec2c52823cacbdcdd4ea5b
Byron Elías Molina Hermosilla <bmolina2@externos.bancoestado.cl>
2024-07-08 16:55:46 -0400

    feat: added requirements implementation

--- controllers/trades.js
+++ controllers/trades.js
@@ -1,1 +1,40 @@
+const Trade = require('../models/trades');
 
+const handleErrors = (response, error) => response.status(500).json({ error: error.message });
+
+const create = async (request, response) => {
+  try {
+    const trade = await Trade.create(request.body);
+    response.status(201).json(trade);
+  } catch (error) {
+    handleErrors(response, error);
+  }
+};
+
+const list = async (_request, response) => {
+  try {
+    const trades = await Trade.findAll({ order: [['id', 'ASC']] });
+    response.status(200).json(trades);
+  } catch (error) {
+    handleErrors(response, error);
+  }
+};
+
+const findById = async (request, response) => {
+  try {
+    const trade = await Trade.findByPk(request.params.id);
+    if (trade) response.status(200).json(trade);
+    response.status(404).send('ID not found');
+  } catch (error) {
+    handleErrors(response, error);
+  }
+};
+
+const handleMethodNotAllowed = (_request, response) => response.status(405).end();
+
+module.exports = {
+  create,
+  list,
+  findById,
+  handleMethodNotAllowed,
+};
--- routes/trades.js
+++ routes/trades.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const TradeController = require('../controllers/trades');
 
+router.post('/', TradeController.create);
+
+router.get('/', TradeController.list);
+
+router.get('/:id', TradeController.findById);
+
+['delete', 'put', 'patch'].forEach(method =>
+  router[method]('/:id', TradeController.handleMethodNotAllowed)
+)
 
 module.exports = router;
